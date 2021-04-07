5ea3313331f49cb5c2bf8354b56b701423df33b1
ducvuong25 <ducvuong25@gmail.com>
2021-04-07 09:59:30 +0700

    for_in

---
+++ for_in.rb
@@ -0,0 +1,5 @@
+intArr = [1,2,3,4,5,6,7]
+
+for i in 0..intArr.length - 1
+    puts "gia tri cua index #{i} lan luot la #{intArr[i]}"
+end
--- while_loop.rb
+++ while_loop.rb
@@ -1,4 +1,6 @@
-int i = 1
-while i < 5
-    sout    
+i = 1
+while i <=5
+    puts "lap lan thu " + i.to_s
+    i = i + 1
+    
 end
